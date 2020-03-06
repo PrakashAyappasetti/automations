4b854134a5693294b8a5a395cfb47fa04cea00e0
ranjithran <36547960+ranjithran@users.noreply.github.com>
2020-03-07 01:21:21 +0530

    Update Rantest1.py

--- result automation/Rantest1.py
+++ result automation/Rantest1.py
@@ -1,41 +1,57 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
 import re
 from bs4 import BeautifulSoup
 import requests
 import json
 import httplib2
 import urllib.request
+from selenium import webdriver
 
-# instantiate a chrome options object so you can set the size and headless preference
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
+url = 'https://jntuaresults.ac.in/'
 
-# download the chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put it in the
-# current directory
-chrome_driver = os.getcwd() +"\\chromedriver.exe"
-# go to Google and click the I'm Feeling Lucky button
-driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-driver.get("https://www.google.com")
-url ='https://jntuaresults.ac.in/view-results-56736241.html'
+try:
+    src = requests.get('https://jntuaresults.ac.in/').text
+except:
+    print('no internet')
+    exit()
+
+soup = BeautifulSoup(src, 'lxml')
+
+table = soup.find('table', {'class': 'ui table segment'})
+
+
+l = ""
+def getRes_a(value1, value2):
+    # geturl = ""
+    for row in soup.findAll("a", href=True):
+        if value1 in row.text:
+            # print(row.text)
+            h = row['href']
+            geturl = f'{url}{h}'
+            print(f'{row.text} : {geturl}')
+            if value2 in row.text:
+                return geturl
+        else :
+            return "result not found or has been deleted by Admin"
+
+
+
+
+l=getRes_a("B.Tech III Year I Semester (R15) ", "June/July 2019")
+print(l)
+
 rollno = '163G1A0505'
-driver.get(url)
-field = driver.find_element_by_xpath('/html/body/div/div[1]/div/div/center/table/tbody/tr/th/center/input[1]')
-btn = driver.find_element_by_xpath('/html/body/div/div[1]/div/div/center/table/tbody/tr/th/center/input[2]')
+browser = webdriver.Chrome()
+# browser = webdriver.PhantomJS()
+print(l)
+browser.get(l)
+field = browser.find_element_by_xpath(
+        '/html/body/div/div[1]/div/div/center/table/tbody/tr/th/center/input[1]')
+btn = browser.find_element_by_xpath(
+        '/html/body/div/div[1]/div/div/center/table/tbody/tr/th/center/input[2]')
 field.send_keys(rollno)
 btn.click()
 print('btn clicked')
-# pr_btn = browser.find_element_by_xpath('/html/body/div/div[1]/div/div/center/div[1]/table/tbody/tr[5]/th/input')
-# pr_btn.click()
-print(driver.current_url)
-driver.get_screenshot_as_file("capture.png")
-driver.close()
-src=requests.get(driver.current_url)
-# print(src.text())
-# soup = BeautifulSoup(src, 'lxml')
-# print(soup.name())
-
-# table = soup.find('table', {'class': 'ui table segment'})
-# capture the screen
+    # pr_btn = browser.find_element_by_xpath('/html/body/div/div[1]/div/div/center/div[1]/table/tbody/tr[5]/th/input')
+    # pr_btn.click()
+    # print(browser.title)
+    # browser.stop_client()
